516166fe095a638aa0cb97852f305b44d6fac043
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-16 00:55:43 +0000

    setted cart app

--- cart/urls.py
+++ cart/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from .views import view_cart, add_to_cart, adjust_cart
 
 
--- cart/views.py
+++ cart/views.py
@@ -5,14 +5,29 @@ def view_cart(request):
     """A view that render the cart contents page """
     return render(request, 'cart.html')
 
-def add_to_cart(request):
+def add_to_cart(request, id):
     """Add a quantity of specific product to the cart """
-    quantity = int(request.post.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
+    print(quantity)
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
     request.session['cart'] = cart
     return redirect(reverse('index'))
 
 def adjust_cart(request, id):
     """Adjust the quantity of a specified product to the specified amount"""
     quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = Quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+
+    return redirect(reverse('view_cart'))
