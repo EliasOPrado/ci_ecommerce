84e8957e07d76e9a1cc07798f4a5edaf97a4d77d
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-22 17:48:14 +0000

    added changes to procfile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'https://shopping-cart-ci.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://shopping-cart-ci.herokuapp.com']
 
 
 # Application definition
